6c9dc3db628d9aee9ef00e1777c86f549d1555d5
ElZakyi <zakariaelfoual0@gmail.com>
2026-02-25 11:09:50 +0100

    Fix Structure

--- src/main/java/com/cihbank/backend/rolepermission/RolePermissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cihbank.backend.rolepermission;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface RolePermissionRepository extends JpaRepository<RolePermission,RolePermissionId> {
-    List<RolePermission> findByRoleIdRole(Integer idRole);
-}
